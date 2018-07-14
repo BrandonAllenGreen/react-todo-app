46ca840ab55f31fe7e97a1f6bd91e4706451baba
BrandonAllenGreen <green04@gmail.com>
2018-07-14 10:25:52 -0500

    saving to push

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
 
 class Todos extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.createTodos = this.createTodos.bind(this);
-  // }
-
   createTodos(todo) {
     return <li key={todo.key}>{todo.text}</li>;
   }
-  klj;
 
   render() {
     const todos = this.props.entries;
